91fbad0ced6ed0472170f42874d385df37744ec1
rmoen <rob.moen@gmail.com>
2011-10-17 14:38:20 -0700

    fixed errors

--- classes/ppbtn.php
+++ classes/ppbtn.php
@@ -13,14 +13,20 @@
  *	
  */
  
- 	class ppBtn{
+ 	class Ppbtn{
  	
- 		$keyfile = Kohana::config('ppbtn.config.keyfile');
- 		$certfile = Kohana::config('ppbtn.config.certfile');
- 		$paypal_certfile =  Kohana::config('ppbtn.config.ppcertfile');
- 		$openssl = Kohana::config('ppbtn.config.openssl');
+ 		public $keyfile;
+ 		public $certfile;
+ 		public $paypal_certfile;
+		public $openssl;
  	
- 		static public function __contruct(){
+		public function __construct(){
+
+			$this->keyfile = Kohana::config('ppbtn.keyfile');
+	 		$this->certfile = Kohana::config('ppbtn.certfile');
+	 		$this->paypal_certfile = Kohana::config('ppbtn.ppcertfile');
+			$this->openssl = Kohana::config('ppbtn.openssl');
+			
  			//make sure some things are set here
 			if (!file_exists($this->keyfile)) {
 				die("key file: {$this->keyfile} not found");
@@ -36,11 +42,11 @@
 			}
  		}
  	
- 		static public function getBtn($item){			
+		public function getBtn($item){			
 			$hash = array('cmd' => '_xclick',
-			        'business' => Kohana::config('ppbtn.config.business'),
-			        'cert_id' => Kohana::config('ppbtn.config.cert_id'),
-			        'lc' => Kohana::config('ppbtn.config.lang'),
+			        'business' => Kohana::config('ppbtn.business'),
+			        'cert_id' => Kohana::config('ppbtn.cert_id'),
+			        'lc' => Kohana::config('ppbtn.lang'),
 			        'custom' => isset($item->custom) ? $item->custom : '',
 			        'invoice' => $item->invoiceID,
 			        'currency_code' => 'USD', 
@@ -56,9 +62,9 @@
 			return $this->returnBtnHTML($encrypted);
  		}	
 
-		static public function paypal_encrypt($hash){
+		public function paypal_encrypt($hash){
 			//Build Notation
-			$hash['bn']= Kohana::config('ppbtn.config.bn').'PHP_EWP2';
+			$hash['bn']= Kohana::config('ppbtn.bn').'PHP_EWP2';
 		
 			$data = "";
 			foreach ($hash as $key => $value) {
@@ -67,10 +73,10 @@
 				}
 			}
 		
-			$openssl_cmd = "({$this->openssl} smime -sign -signer {$this->certfile} -inkey {$this->keyfile} " .
-								"-outform der -nodetach -binary <<_EOF_\n{$data}\n_EOF_\n) | " .
-								"{$this->openssl} smime -encrypt -des3 -binary -outform pem {$this->paypal_certfile}";
-		
+			$openssl_cmd = "(".$this->openssl." smime -sign -signer ".$this->certfile." -inkey ".$this->keyfile." " .
+								"-outform der -nodetach -binary <<_EOF_\n".$data."\n_EOF_\n) | " .
+								$this->openssl." smime -encrypt -des3 -binary -outform pem ".$this->paypal_certfile;
+			echo $openssl_cmd;
 			exec($openssl_cmd, $output, $error);
 		
 			if (!$error) {
@@ -80,16 +86,18 @@
 			}
 		}
 		
-		static public function returnBtnHTML($encrypted){
-$form = <<<PPFORM
-			<form action='https://www.paypal.com/cgi-bin/webscr' method='post' target=_blank>
-			<input type='hidden" name="cmd" value="_s-xclick'>
-			<input type='hidden" name="encrypted" value="{$encrypted}">
-			<input type="submit" value="Pay">
-			</form>		
-PPFORM;	
+		public function returnBtnHTML($encrypted){
+
+$form = "
+<form action='https://www.paypal.com/cgi-bin/webscr' method='post' target=_blank>
+<input type='hidden' name='cmd' value='_s-xclick'>
+<input type='hidden' name='encrypted' value='{$encrypted}'>
+<input type='submit' value='Pay'>
+</form>		
+";
+			
+
 			return $form;			
 		}
- 	}
- 	
+ 	} //end class
 ?>
--- config/ppbtn.php
+++ config/ppbtn.php
@@ -2,14 +2,12 @@
 
 //use APPPATH to ref application directory 
 return array(
-    'config' => array(
-		'lang' 	     => 'US',
-		'openssl'    => '/usr/bin/openssl',
-    	'bn'         => 'CompanyName',  //build notation
-    	'business'   => 'seller@domain.com',  //seller account
-		'cert_id'    => '1234567890123', // cert id, obtained from paypal.
-        'keyfile'    => APPPATH.'/paypal/company-key.pem', //location of required cert / key files
-        'certfile'   => APPPATH.'/paypal/company-cert.pem', //location of cert
-        'ppcertfile' => APPPATH.'/paypal/paypal-cert.pem' //location of signed paypal public certificate
-    ),
+	'lang' 	     => 'US',
+	'openssl'    => '/usr/bin/openssl',
+	'bn'         => 'CompanyName',  //build notation
+	'business'   => 'seller@domain.com',  //seller account
+	'cert_id'    => '1234', // cert id, obtained from paypal.
+    'keyfile'    => APPPATH.'/vendor/paypalcerts/company-key.pem', //location of required cert / key files
+    'certfile'   => APPPATH.'/vendor/paypalcerts/company-cert.pem', //location of cert
+    'ppcertfile' => APPPATH.'/vendor/paypalcerts/company-paypal-cert.pem' //location of signed paypal public certificate
 );
